7333baeb554042ee00b2b4e46f61fd3d50197335
BMW060 <brennan.m.wood@gmail.com>
2025-04-05 18:16:42 +0000

    Major-Year

--- ProcessData.py
+++ ProcessData.py
@@ -1,7 +1,7 @@
 #ProcessData.py
-#Name:
-#Date:
-#Assignment:
+#Name: Brennan Wood
+#Date: 4/5/25 
+#Assignment: Lab 8 Process Data
 
 import random
 
@@ -12,6 +12,41 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+
+  def userID(firstName,lastName,idnum):
+    if len(lastName) < 5:
+      id = firstName[0] + lastName + "x"*(5-len(lastName)) + idnum[len(idnum)-3:]
+    else:
+      id = firstName[0] + lastName + idnum[len(idnum)-3:]
+    return id
+  
+  def schyear(yr):
+    if yr == "Freshman":
+      year = "FR"
+    elif yr == "Sophomore":
+      year = "SO"
+    elif yr == "Junior":
+      year = "JR"
+    else:
+      year = "SR"
+    return year
+
+  for line in inFile:
+    data = line.split()
+    firstName = data[0]
+    lastName = data[1]
+    email = data[2]
+    idnum = data[3]
+    bday = data[4]
+    yr = data[5]
+    maj = data[6]
+    student_id = userID(firstName,lastName,idnum)
+    year = schyear(yr)
+    
+    output = lastName + "," + firstName + "," + student_id + "," + maj[:3] + "-" + year + "\n"
+    outFile.write(output)
+
+
 
 
 
